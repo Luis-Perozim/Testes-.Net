b95cf4fd6cb3fa47142e197180de3cf93719aba0
Luis Felipe Perozim do Nascimento <lnascimento.sjp@ecori.com.br>
2023-08-02 10:11:27 -0300

    Adição do exercíco de agregação

---
+++ Agregação/Professor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregação;
+
+internal class Professor
+{
+    public string? Nome { get; set; }
+    public string? Disciplina { get; set; }
+    public Professor(string? nome, string? disciplina)
+    {
+        Nome = nome;
+        Disciplina = disciplina;
+    }
+}
